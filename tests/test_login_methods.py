b0a483e150b54b9d0c47857d285a0f4355b18271
dona126 <donamariamsunny@gmail.com>
2026-05-13 00:17:59 +0530

    update

--- tests/test_login_methods.py
+++ tests/test_login_methods.py
@@ -1,16 +1,16 @@
 from playwright.sync_api import Page, expect
-import pages.login_page
+from pages.login_page import LoginPage
 
 def test_login_button_visible(page : Page):
     #Checks Login button is visible in Login Page
-    login = pages.login_page.LoginPage(page)
+    login = LoginPage(page)
     login.goto()
     expect(login.is_login_button_visible()).to_be_visible()
 
 
 def test_get_page_title(page : Page):
     #Verify the title and prints it, of Login Page
-    login = pages.login_page.LoginPage(page)
+    login = LoginPage(page)
     login.goto()
     expect(page).to_have_title("Swag Labs")
 
